6306f60e3486f85d57821ddb2b9e3dd3896ff2a3
graphicsxp <graphicsx@gmail.com>
2017-03-15 10:14:11 +0100

    added login/signup

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,10 @@
 import { Component, ViewChild } from '@angular/core';
-import { Nav, Platform } from 'ionic-angular';
+import { Nav, Platform, AlertController } from 'ionic-angular';
 import { StatusBar, Splashscreen } from 'ionic-native';
+import { Auth, User } from '@ionic/cloud-angular';
+import { AppVersion } from 'ionic-native';
 
-import { Page1 } from '../pages/page1/page1';
-import { Page2 } from '../pages/page2/page2';
-
+import { LoginComponent } from '../pages/login/login.component';
 
 @Component({
   templateUrl: 'app.html'
@@ -12,27 +12,39 @@ import { Page2 } from '../pages/page2/page2';
 export class MyApp {
   @ViewChild(Nav) nav: Nav;
 
-  rootPage: any = Page1;
+  rootPage: any = LoginComponent;
 
-  pages: Array<{title: string, component: any}>;
+  pages: Array<{ title: string, component: any }>;
+  username: string;
+  app: any = {
+    name: '',
+    versionCode: '',
+    versionNumber: '',
+    packageName: ''
+  };
 
-  constructor(public platform: Platform) {
+  constructor(public platform: Platform, private _auth: Auth, public _user: User, private _alertController: AlertController) {
     this.initializeApp();
-
-    // used for an example of ngFor and navigation
-    this.pages = [
-      { title: 'Page One', component: Page1 },
-      { title: 'Page Two', component: Page2 }
-    ];
-
   }
 
   initializeApp() {
     this.platform.ready().then(() => {
+
+      this.username = this._user.details.username;
+
       // Okay, so the platform is ready and our plugins are available.
       // Here you can do any higher level native things you might need.
+      AppVersion.getAppName().then(v => this.app['name'] = v);
+      AppVersion.getVersionCode().then(v => this.app['versionCode'] = v);
+      AppVersion.getVersionNumber().then(v => this.app['versionNumber'] = v);
+      AppVersion.getPackageName().then(v => this.app['packageName'] = v);
+
       StatusBar.styleDefault();
-      Splashscreen.hide();
+      if (Splashscreen) {
+        setTimeout(() => {
+          Splashscreen.hide();
+        }, 100);
+      }
     });
   }
 
@@ -40,5 +52,26 @@ export class MyApp {
     // Reset the content nav to have just this page
     // we wouldn't want the back button to show in this scenario
     this.nav.setRoot(page.component);
+  }
+
+  logout() {
+
+    let confirm = this._alertController.create({
+      title: 'Confirm',
+      message: 'Are you sure you want to logout ?',
+      buttons: [
+        {
+          text: 'Cancel',
+        },
+        {
+          text: 'Ok',
+          handler: () => {
+            this._auth.logout();
+            this.nav.setRoot(LoginComponent);
+          }
+        }
+      ]
+    });
+    confirm.present();
   }
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,23 +1,30 @@
+import { SignupModule } from './../pages/signup/signup.module';
+import { LoginModule } from './../pages/login/login.module';
 import { NgModule, ErrorHandler } from '@angular/core';
+import { CloudSettings, CloudModule } from '@ionic/cloud-angular';
+
 import { IonicApp, IonicModule, IonicErrorHandler } from 'ionic-angular';
 import { MyApp } from './app.component';
-import { Page1 } from '../pages/page1/page1';
-import { Page2 } from '../pages/page2/page2';
+
+const cloudSettings: CloudSettings = {
+  'core': {
+    'app_id': '086cd9b3'
+  }
+};
 
 @NgModule({
   declarations: [
-    MyApp,
-    Page1,
-    Page2
+    MyApp
   ],
   imports: [
-    IonicModule.forRoot(MyApp)
+    LoginModule,
+    SignupModule,
+    IonicModule.forRoot(MyApp),
+    CloudModule.forRoot(cloudSettings)
   ],
   bootstrap: [IonicApp],
   entryComponents: [
-    MyApp,
-    Page1,
-    Page2
+    MyApp
   ],
   providers: [{provide: ErrorHandler, useClass: IonicErrorHandler}]
 })
